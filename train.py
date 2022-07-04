9c0cab97adb9caf39f9c297e1669950b072101c2
neetmehta <neetmehta19@gmail.com>
2022-07-03 21:21:47 -0400

    dataset path

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ print('seed created')
 
 # APPLY_AUG = True
 # PRETRAINED = True
-ROOT = r"E:\Deep Learning Projects\datasets\kitti_raw"
+ROOT = r"/dataset/kitti_raw_image"
 BATCH_SIZE = 2
 LEARNING_RATE = 1e-4
 NUM_WORKERS = 0
